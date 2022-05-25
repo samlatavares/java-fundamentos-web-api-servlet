39916b2bed641771504dd66e2850d9c03ca22801
Samla Azevedo Tavares <samla_azevedo@outlook.com>
2022-05-25 18:36:29 -0300

    Processing date field in the servlet

--- gerenciador/WebContent/formNovaEmpresa.jsp
+++ gerenciador/WebContent/formNovaEmpresa.jsp
@@ -10,6 +10,7 @@
 	<body>		
 		<form action="${linkServletNovaEmpresa}" method="post">
 			Nome: <input type="text" name="nome"/>
+			Data Abertura: <input type="text" name="data"/>
 			<input type="submit"/>
 		</form>
 	</body>
--- gerenciador/src/main/java/br/com/samla/gerenciador/servlet/NovaEmpresaServlet.java
+++ gerenciador/src/main/java/br/com/samla/gerenciador/servlet/NovaEmpresaServlet.java
@@ -2,6 +2,9 @@ package br.com.samla.gerenciador.servlet;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -19,8 +22,20 @@ public class NovaEmpresaServlet extends HttpServlet {
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String nomeEmpresa = request.getParameter("nome");
+		String dataAberturaParam = request.getParameter("data");		
+		
+		Date dataAbertura = null;
+		try {
+			SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+			dataAbertura = sdf.parse(dataAberturaParam);
+		} catch (ParseException e) {
+			e.printStackTrace();
+			throw new ServletException(e);
+		}
+		
 		Empresa empresa = new Empresa();
 		empresa.setNome(nomeEmpresa);
+		empresa.setDataAbertura(dataAbertura);
 		
 		Banco banco = new Banco();
 		banco.adicionar(empresa);
@@ -28,6 +43,7 @@ public class NovaEmpresaServlet extends HttpServlet {
 		//Chamando JSP
 		RequestDispatcher rd = request.getRequestDispatcher("novaEmpresaCriada.jsp");
 		request.setAttribute("nomeEmpresa", empresa.getNome());
+		request.setAttribute("dataAbertura", empresa.getDataAbertura());
 		rd.forward(request, response);
 	}
 
