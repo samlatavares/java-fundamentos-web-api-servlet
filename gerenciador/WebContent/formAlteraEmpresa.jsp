e77cd1f76a097be13f4bcef9c9cc93f02c69ab92
Samla Azevedo Tavares <samla_azevedo@outlook.com>
2022-06-01 11:54:18 -0300

    Implementing edit

--- gerenciador/WebContent/formAlteraEmpresa.jsp
+++ gerenciador/WebContent/formAlteraEmpresa.jsp
@@ -1,6 +1,6 @@
 <%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
 <%@ taglib uri="http://java.sun.com/jsp/jstl/fmt" prefix="fmt"%>
-<c:url value="/alteraEmpresa" var="linkServletAlteraEmpresa"/>
+<c:url value="/alteraEmpresa" var="linkServletNovaEmpresa"/>
 
 <!DOCTYPE html>
 <html>
@@ -12,6 +12,7 @@
 		<form action="${linkServletNovaEmpresa}" method="post">
 			Nome: <input type="text" name="nome" value="${empresa.nome}"/>
 			Data Abertura: <input type="text" name="data" value="<fmt:formatDate value="${empresa.dataAbertura}" pattern="dd/MM/yyyy"/>" />
+			<input type="hidden" name="id" value="${empresa.id }"/>
 			<input type="submit"/>
 		</form>
 	</body>
---
+++ gerenciador/src/main/java/br/com/samla/gerenciador/servlet/AlteraEmpresaServlet.java
@@ -0,0 +1,41 @@
+package br.com.samla.gerenciador.servlet;
+
+import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/alteraEmpresa")
+public class AlteraEmpresaServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String nomeEmpresa = request.getParameter("nome");
+		String dataAberturaParam = request.getParameter("data");
+		String idParam = request.getParameter("id");
+		Integer id = Integer.valueOf(idParam);
+		
+		Date dataAbertura = null;
+		try {
+			SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+			dataAbertura = sdf.parse(dataAberturaParam);
+		} catch (ParseException e) {
+			e.printStackTrace();
+			throw new ServletException(e);
+		}
+		
+		Banco banco = new Banco();
+		Empresa empresa = banco.buscarEmpresa(id);
+		empresa.setDataAbertura(dataAbertura);
+		empresa.setNome(nomeEmpresa);
+		
+		response.sendRedirect("listaEmpresas");
+	}
+
+}
