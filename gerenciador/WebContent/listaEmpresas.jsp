1f6e87682479baffb446f0b9e9b5eb4c2eeb5350
Samla Azevedo Tavares <samla_azevedo@outlook.com>
2022-05-31 22:33:02 -0300

    Implementing remove method

--- gerenciador/WebContent/listaEmpresas.jsp
+++ gerenciador/WebContent/listaEmpresas.jsp
@@ -22,7 +22,10 @@
 		<br/> Lista de empresas: <br/>
 		<ul>
 			<c:forEach items="${empresas}" var="novaEmpresa">		
-				<li>${novaEmpresa.nome} - <fmt:formatDate value="${novaEmpresa.dataAbertura}" pattern="dd/MM/yyyy"/></li>
+				<li>
+					${novaEmpresa.nome} - <fmt:formatDate value="${novaEmpresa.dataAbertura}" pattern="dd/MM/yyyy"/>
+					<a href="/gerenciador/removeEmpresa?id=${novaEmpresa.id}">remover</a>
+				</li>
 			</c:forEach>		
 		</ul>
 	</body>
--- gerenciador/src/main/java/br/com/samla/gerenciador/servlet/Banco.java
+++ gerenciador/src/main/java/br/com/samla/gerenciador/servlet/Banco.java
@@ -1,29 +1,47 @@
 package br.com.samla.gerenciador.servlet;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 public class Banco {
 	
 	private static List<Empresa> listaEmpresas = new ArrayList<>();
+	private static Integer chaveSequencial= 1;
 	
 	static {
 		Empresa empresa = new Empresa();
+		empresa.setId(chaveSequencial++);
 		empresa.setNome("Alura");
-		
+
 		Empresa empresa2 = new Empresa();
+		empresa2.setId(chaveSequencial++);
 		empresa2.setNome("Caelum");
 		
 		listaEmpresas.add(empresa);
 		listaEmpresas.add(empresa2);
 	}
-
-	public void adicionar(Empresa empresa) {
-		listaEmpresas.add(empresa);		
-	}
 	
 	public List<Empresa> getListaEmpresas() {
 		return Banco.listaEmpresas;
+	}
+
+	public void adicionar(Empresa empresa) {
+		empresa.setId(chaveSequencial++);
+		listaEmpresas.add(empresa);
+	}
+	
+
+	public void removerEmpresa(Integer id) {
+		Iterator<Empresa> it = listaEmpresas.iterator();
+		
+		while(it.hasNext()) {
+			Empresa emp = it.next();
+			
+			if(emp.getId().equals(id)) {
+				it.remove();
+			}
+		}	
 	}
 
 }
